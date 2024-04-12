6f41049aa6472983c277a68a47c665901274b40b
anand <vermaanand278@gmail.com>
2024-04-12 12:33:44 +0530

    dsfa

--- controller/Aviator/aviator.js
+++ controller/Aviator/aviator.js
@@ -1,23 +1,13 @@
-const { json } = require("express");
 const con = require("../../config/database");
 
 exports.promotionCount = async (req, res) => {
-  // con.connect();
   const { id } = req.query;
-  if (!id) {
+  if (!id || isNaN(id)) {
     return res.status(400).json({
-      message: "Id is missing",
+      message: "Id is missing or invalid",
     });
   }
 
-  // Check if the provided ID is a valid number
-  if (isNaN(id)) {
-    return res.status(400).json({
-      message: "Invalid ID format",
-    });
-  }
-
-  let array = [];
   try {
     con.query("SELECT * FROM user", (err, result) => {
       if (err) {
@@ -28,21 +18,22 @@ exports.promotionCount = async (req, res) => {
           er: err,
         });
       }
-      // console.log(result);
-      array = result;
-      // console.log(array, "This is final result");
 
-      array = array?.map((i) => {
-        return { ...i, count: 0, teamcount: 0, directReferrals: [] };
-      });
+      const array = result.map((i) => ({
+        ...i,
+        count: 0,
+        teamcount: 0,
+        directReferrals: [],
+      }));
 
-      let new_data = updateReferralCountnew(array)?.find((i) => i.id == id);
-      let levels = Array.from({ length: 20 }, (_, i) => `level_${i + 1}`);
-   
-      // Add more levels as needed
+      let new_data = updateReferralCountnew(array).find((i) => i.id == id);
+      const levels = Array.from({ length: 20 }, (_, i) => `level_${i + 1}`);
 
-      for (let i =levels.length - 1; i >= 0; i--) {
-        let currentLevel = new_data?.teamMembersByLevel[levels[i-1]];
+      let direct_ids = new_data.directReferrals?.map((i) => i?.c_id);
+
+      let indirect_ids = [];
+      for (let i = levels.length - 1; i >= 0; i--) {
+        let currentLevel = new_data?.teamMembersByLevel[levels[i - 1]];
         let nextLevel = new_data?.teamMembersByLevel[levels[i]];
 
         if (currentLevel && nextLevel) {
@@ -54,16 +45,97 @@ exports.promotionCount = async (req, res) => {
         }
       }
 
-      if (result && result.length > 0) {
-        return res.status(200).json({
-          data: new_data,
-          msg: "Data fetched successfully",
-        });
-      } else {
-        return res.status(404).json({
-          msg: "No data found",
-        });
+      for (let i = 1; i <= 20; i++) {
+        if (new_data.teamMembersByLevel[`level_${i}`]?.length > 0) {
+          indirect_ids.push(
+            ...new_data.teamMembersByLevel[`level_${i}`].map((item) => item.id)
+          );
+        }
       }
+
+      new_data = { ...new_data, deposit_member_amount: [] };
+
+      const promises = [];
+      for (let i = 1; i <= 20; i++) {
+        if (new_data.teamMembersByLevel[`level_${i}`]?.length > 0) {
+          let levelIds = new_data.teamMembersByLevel[`level_${i}`].map(
+            (k) => k.id
+          );
+          const promise = new Promise((resolve, reject) => {
+            con.query(
+              `SELECT SUM(tr15_amt) AS total_amount,count(*) AS total_member FROM tr15_fund_request WHERE tr15_status = 'Success' AND tr15_depo_type = 'Winzo' AND tr15_uid IN (${levelIds.join(
+                ","
+              )});`,
+              (err, resultteamamount) => {
+                if (err) {
+                  console.error(err);
+                  reject(err);
+                } else {
+                  resolve(resultteamamount[0].total_amount || 0);
+                }
+              }
+            );
+          });
+          promises.push(promise);
+        } else {
+          promises.push(0);
+        }
+      }
+
+      Promise.all(promises)
+        .then((deposit_member_amounts) => {
+          new_data.deposit_member_amount = deposit_member_amounts;
+          con.query(
+            `SELECT SUM(tr15_amt) AS total_amount,count(*) AS total_member FROM tr15_fund_request WHERE tr15_status = 'Success' AND tr15_depo_type = 'Winzo' AND tr15_uid IN (${direct_ids.join(
+              ","
+            )});`,
+            (err, result) => {
+              if (err) {
+                console.error(err);
+                return res.status(500).json({
+                  msg: "Error in data fetching",
+                  error: err.message,
+                  er: err,
+                });
+              }
+
+              con.query(
+                `SELECT SUM(tr15_amt) AS total_amount,count(*) AS total_member FROM tr15_fund_request WHERE tr15_status = 'Success' AND tr15_depo_type = 'Winzo' AND tr15_uid IN (${indirect_ids.join(
+                  ","
+                )});`,
+                (err, resultteam) => {
+                  if (err) {
+                    console.error(err);
+                    return res.status(500).json({
+                      msg: "Error in data fetching",
+                      error: err.message,
+                      er: err,
+                    });
+                  }
+
+                  return res.status(200).json({
+                    data: {
+                      ...new_data,
+                      deposit_member: result[0].total_member || 0,
+                      deposit_recharge: result[0].total_amount || 0,
+                      deposit_member_team: resultteam[0].total_member || 0,
+                      deposit_recharge_team: resultteam[0].total_amount || 0,
+                    },
+                    msg: "Data fetched successfully",
+                  });
+                }
+              );
+            }
+          );
+        })
+        .catch((err) => {
+          console.error(err);
+          return res.status(500).json({
+            msg: "Error in data fetching",
+            error: err.message,
+            er: err,
+          });
+        });
     });
   } catch (e) {
     console.error(e);
@@ -72,25 +144,16 @@ exports.promotionCount = async (req, res) => {
       error: e.message,
     });
   }
-  // con.release();
 };
 
 function updateReferralCountnew(users) {
   const countMap = {};
   const teamCountMap = {};
-  const depositMemberMap = {}; // Map to store the count of direct referrals with first_recharge = 1
-  const depositMemberTeamMap = {}; // Map to store the count of direct and indirect referrals with first_recharge = 1
-  const depositRechargeMap = {}; // Map to store the total sum of recharge amounts for direct referrals
-  const depositRechargeTeamMap = {}; // Map to store the total sum of recharge amounts for direct and indirect referrals
 
   // Initialize count for each user
   users.forEach((user) => {
     countMap[user.id] = 0;
     teamCountMap[user.id] = 0;
-    depositMemberMap[user.id] = 0;
-    depositMemberTeamMap[user.id] = 0;
-    depositRechargeMap[user.id] = 0;
-    depositRechargeTeamMap[user.id] = 0;
     user.directReferrals = []; // Initialize directReferrals array for each user
   });
 
@@ -102,16 +165,6 @@ function updateReferralCountnew(users) {
       countMap[user.referral_user_id]++;
     }
   });
-  // Update deposit_recharge and deposit_recharge_team for each user recursively
-  const updateRechargeRecursively = (user) => {
-    let totalRecharge = user.recharge;
-    users.forEach((u) => {
-      if (u.referral_user_id === user.id) {
-        totalRecharge += updateRechargeRecursively(u);
-      }
-    });
-    return totalRecharge;
-  };
 
   // Update team count, deposit_member, and deposit_member_team count for each user recursively
   const updateTeamCountRecursively = (user) => {
@@ -153,33 +206,6 @@ function updateReferralCountnew(users) {
     // Update teamCountMap if user.id exists in countMap
     if (countMap.hasOwnProperty(user.id)) {
       teamCountMap[user.id] = updateTeamCountRecursively(user);
-    }
-
-    // Update deposit_member count for direct referrals
-    if (user.referral_user_id !== null && user.recharge > 0) {
-      if (depositMemberMap.hasOwnProperty(user.referral_user_id)) {
-        depositMemberMap[user.referral_user_id]++;
-      }
-    }
-
-    // Update deposit_member_team count for direct and indirect referrals
-    if (user.recharge === 1) {
-      if (countMap.hasOwnProperty(user.id)) {
-        depositMemberTeamMap[user.id] = teamCountMap[user.id];
-      }
-    }
-
-    // Update deposit_recharge for direct referrals
-    if (user.referral_user_id !== null) {
-      if (depositRechargeMap.hasOwnProperty(user.referral_user_id)) {
-        depositRechargeMap[user.referral_user_id] += user.recharge;
-      }
-    }
-
-    // Update deposit_recharge_team recursively
-    if (countMap.hasOwnProperty(user.id)) {
-      depositRechargeTeamMap[user.id] =
-        user.recharge + updateRechargeRecursively(user);
     }
 
     // Add direct referral to the user's directReferrals array
@@ -224,18 +250,6 @@ function updateReferralCountnew(users) {
     user.count = countMap.hasOwnProperty(user.id) ? countMap[user.id] : 0;
     user.teamcount = teamCountMap.hasOwnProperty(user.id)
       ? teamCountMap[user.id]
-      : 0;
-    user.deposit_member = depositMemberMap.hasOwnProperty(user.id)
-      ? depositMemberMap[user.id]
-      : 0;
-    user.deposit_member_team = depositMemberTeamMap.hasOwnProperty(user.id)
-      ? depositMemberTeamMap[user.id]
-      : 0;
-    user.deposit_recharge = depositRechargeMap.hasOwnProperty(user.id)
-      ? depositRechargeMap[user.id]
-      : 0;
-    user.deposit_recharge_team = depositRechargeTeamMap.hasOwnProperty(user.id)
-      ? depositRechargeTeamMap[user.id]
       : 0;
   });
   return users;
